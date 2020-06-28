8ad6dfc6cccf1cce1bd1c73574fda7809ea2bd2d
Katheeskumar Sithamparapillai <katheeskumar@outlook.com>
2020-06-28 06:37:43 +0530

    added some files

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -8,6 +8,14 @@ module.exports = {
             { text: 'Docs', link: '/docs/' },
             { text: 'GitLeaf', link: 'https://gitleaf.com' }
         ],
+        sidebar: {
+           '/docs/': [
+               '',
+              'Introduction',
+              'Getting Started',
+              'Commands' 
+           ]
+        }
     },
     plugins: ['@vuepress/back-to-top']
 }
---
+++ docs/docs/Commands.md
@@ -0,0 +1,12 @@
+# Commands
+
+to check opshit version
+
+```bash
+node opshit -v
+```
+
+to create Controller
+```bash
+node opshit make:ctrl <ControllerName>
+```
